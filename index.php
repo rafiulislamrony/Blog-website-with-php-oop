6cfcafdf3785be7b680bbdf651ab5def3419dcb4
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-03 06:52:10 +0600

    Blog with php opp

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/slider.php'; ?>
+ 
+	<div class="contentsection contemplete clear">
+
+		<div class="maincontent clear">
+			<div class="samepost clear">
+				<h2><a href="">Our post title here</a></h2>
+				<h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
+				<a href="#"><img src="images/post1.jpg" alt="post image" /></a>
+				<p>
+					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go
+					here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be
+					go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text
+					will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some
+					text will be go here.Some text will be go here.Some text will be go here.
+				</p>
+				<div class="readmore clear">
+					<a href="post.php">Read More</a>
+				</div>
+			</div>
+			<div class="samepost clear">
+				<h2><a href="">Our post title here</a></h2>
+				<h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
+				<img src="images/post2.png" alt="post image" />
+				<p>
+					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go
+					here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be
+					go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text
+					will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some
+					text will be go here.Some text will be go here.Some text will be go here.
+				</p>
+				<div class="readmore clear">
+					<a href="post.php">Read More</a>
+				</div>
+			</div>
+			<div class="samepost clear">
+				<h2>Our post title here</h2>
+				<h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
+				<img src="images/post1.jpg" alt="post image" />
+				<p>
+					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go
+					here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be
+					go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text
+					will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some
+					text will be go here.Some text will be go here.Some text will be go here.
+				</p>
+				<div class="readmore clear">
+					<a href="post.php">Read More</a>
+				</div>
+			</div>
+
+		</div>
+ 
+		<?php include 'inc/sidebar.php'; ?>
+
+		<?php include 'inc/footer.php'; ?>
