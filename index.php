04d245aa1bde28718bda71cbb02a179b14049ae9
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-03 09:19:34 +0600

    Blog Pagination

--- index.php
+++ index.php
@@ -12,13 +12,24 @@ $fm = new Format();
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 
-		<?php 
-		$query = "SELECT * FROM tbl_post limit 3"; 
-		$post = $db->select($query); 
-		if($post) {
-		while($result = $post->fetch_assoc()) {
-
+		<!-- Pagination -->
+		<?php  
+		 $per_page = 3;
+		 if(isset($_GET["page"])){
+			$page = $_GET["page"];
+		 }else{
+				$page = 1;
+			} 
+		 $start_form =($page-1)*$per_page;
 		?>
+
+		<?php 
+		$query = "SELECT * FROM tbl_post limit $start_form, $per_page "; 
+		$post = $db->select($query); 
+		if($post)  
+		while($result = $post->fetch_assoc()) {
+		?> <!--- Start If -->
+
 		<div class="samepost clear">
 	
 			<h2><a href="post.php?id=<?php echo $result['id'] ?>"><?php echo $result['title'] ?> </a></h2>
@@ -31,9 +42,32 @@ $fm = new Format();
 				<a href="post.php?id=<?php echo $result['id'] ?>">Read More</a>
 				</div>
 			</div>
-		<?php }?> <!--- End While Loop -->
+			<?php
+				// End of the loop content
+			} else {
+			header("Location:404.php");
+			exit;  
+		   }
+		?><!--- End While Loop -->
 
-	   <?php }else{ header("Location:404.php"); } ?>
+		<!--- Pagination -->
+		<?php 
+		$query = "SELECT * FROM tbl_post"; 
+		$result = $db->select($query);
+
+		$total_rows = mysqli_num_rows($result);
+		
+		$total_pages = ceil($total_rows/$per_page);
+		
+		echo "<span class='pagination'> <a href='index.php?page=1'>".'First Page'." </a> ";
+		
+		for ($i = 1; $i < $total_pages; $i++) {
+			echo "<a href='index.php?page=" . $i . "'>" . $i . "</a>";
+		}
+
+       echo "<a href='index.php?page=$total_pages'>".'Last Page'." </a></span>" ?>
+		<!--- Pagination --> 
+ 
 
 		</div>
 		<?php include 'inc/sidebar.php'; ?>
