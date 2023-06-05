a95db571c7bbeb1631a6d0c64c9242239acf8252
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-05 11:00:48 +0600

    Update Copyright Option

--- admin/copyright.php
+++ admin/copyright.php
@@ -1,29 +1,65 @@
 ﻿<?php include 'inc/header.php'; ?>
 <?php include 'inc/sidebar.php'; ?>
-    <div class="grid_10">
+<div class="grid_10">
 
-        <div class="box round first grid">
-            <h2>Update Copyright Text</h2>
-            <div class="block copyblock"> 
-                <form>
-                <table class="form">					
-                    <tr>
-                        <td>
-                            <input type="text" placeholder="Enter Copyright Text..." name="copyright" class="large" />
-                        </td>
-                    </tr>
-                    
-                        <tr> 
-                        <td>
-                            <input type="submit" name="submit" Value="Update" />
-                        </td>
-                    </tr>
-                </table>
-                </form>
-            </div>
+    <div class="box round first grid">
+        <h2>Update Copyright Text</h2>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $note = $fm->validation($_POST['note']);
+
+            $note = $db->link->real_escape_string($note);
+
+            if ($note == "") {
+                echo "<span class='error'>Field Must not be empty.</span>";
+            } else {
+
+                $query = "UPDATE tbl_footer
+                        SET
+                        note='$note'
+                        WHERE id='1'";
+
+                $updated_rows = $db->update($query);
+                if ($updated_rows) {
+                    echo "<span class='success'>Data Updated successfully.</span>";
+                } else {
+                    echo "<span class='error'>Failed to Update data.</span>";
+                }
+            }
+        }
+        ?>
+
+        <div class="block copyblock">
+            <?php
+                $query = "SELECT * FROM tbl_footer WHERE id='1' ";
+                $copyright = $db->select($query);
+                if ($copyright) {
+                    while ($result = $copyright->fetch_assoc()) {
+
+            ?>
+
+                    <form action="copyright.php" method="post">
+                        <table class="form">
+                            <tr>
+                                <td>
+                                    <input type="text" value="<?php echo $result['note'] ?>" name="note" class="large" />
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <input type="submit" name="submit" Value="Update" />
+                                </td>
+                            </tr>
+                        </table>
+
+                    </form>
+                <?php }
+            } ?>
         </div>
     </div>
-    <div class="clear">
- </div>
+</div>
+<div class="clear">
+</div>
 </div>
 <?php include 'inc/footer.php'; ?>
--- inc/footer.php
+++ inc/footer.php
@@ -1,35 +1,48 @@
- 
-	<div class="footersection templete clear">
-		<div class="footermenu clear">
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Contact</a></li>
-				<li><a href="#">Privacy</a></li>
-			</ul>
-		</div>
-		<p>&copy; Copyright Training with live project.</p>
+<div class="footersection templete clear">
+	<div class="footermenu clear">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Privacy</a></li>
+		</ul>
 	</div>
-	
-	<div class="fixedicon clear">
 	<?php
-				$query = "SELECT * FROM tbl_social WHERE id='1' ";
-				$blog_title = $db->select($query);
-				if($blog_title) {
-					while($result = $blog_title->fetch_assoc()) {
+		$query = "SELECT * FROM tbl_footer WHERE id='1' ";
+		$copyright = $db->select($query);
+		if ($copyright) {
+			while ($result = $copyright->fetch_assoc()) {
 
-                ?>
-				 
-				
-		<a href="<?php echo $result['fb']?>" target="_blank"><img src="images/fb.png" alt="Facebook" /></a>
-		<a href="<?php echo $result['tw']?>" target="_blank"><img src="images/tw.png" alt="Twitter" /></a>
-		<a href="<?php echo $result['ln']?>" target="_blank"><img src="images/in.png" alt="LinkedIn" /></a>
-		<a href="<?php echo $result['gp']?>" target="_blank"><img src="images/gl.png" alt="GooglePlus" /></a>
-		<?php } }?>
-	</div>
+			?>
+			<p>&copy; <?php 
+			echo $result['note'] ;
+			echo date('Y');
+			 ?>  
+			</p>
+		<?php }
+	} ?>
+</div>
+
+<div class="fixedicon clear">
+	<?php
+	$query = "SELECT * FROM tbl_social WHERE id='1' ";
+	$blog_title = $db->select($query);
+	if ($blog_title) {
+		while ($result = $blog_title->fetch_assoc()) {
+
+			?>
 
 
-	<script type="text/javascript" src="js/scrolltop.js"></script>
+			<a href="<?php echo $result['fb'] ?>" target="_blank"><img src="images/fb.png" alt="Facebook" /></a>
+			<a href="<?php echo $result['tw'] ?>" target="_blank"><img src="images/tw.png" alt="Twitter" /></a>
+			<a href="<?php echo $result['ln'] ?>" target="_blank"><img src="images/in.png" alt="LinkedIn" /></a>
+			<a href="<?php echo $result['gp'] ?>" target="_blank"><img src="images/gl.png" alt="GooglePlus" /></a>
+		<?php }
+	} ?>
+</div>
+
+
+<script type="text/javascript" src="js/scrolltop.js"></script>
 </body>
 
 </html>
