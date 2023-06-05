f1ee5ee52908c37ddb0b6fcecb04d0c5db20a6e3
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-05 22:29:20 +0600

    Send Messages to Seen Box

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,7 +1,25 @@
 ﻿<?php include 'inc/header.php'; ?>
 <?php include 'inc/sidebar.php'; ?>
+
 <div class="grid_10">
     <div class="box round first grid">
+        <?php
+        if (isset($_GET['seenid'])) {
+            $seenid = $_GET['seenid'];
+            $query = "UPDATE tbl_contact
+            SET status='1'
+            WHERE id='$seenid'";
+
+            $updated_row = $db->update($query);
+
+            if ($updated_row) {
+                echo "<span class='success'>Message Send in the seen Box.</span>";
+
+            } else {
+                echo "<span class='error'>Something Wrong.</span>";
+            }
+        }
+        ?>
         <h2>Inbox</h2>
         <div class="block">
             <table class="data display datatable" id="example">
@@ -16,30 +34,42 @@
                     </tr>
                 </thead>
                 <tbody>
-                <?php 
-                   $query = "SELECT * FROM tbl_contact WHERE status='0' ORDER BY id DESC";
+                    <?php
+                    $query = "SELECT * FROM tbl_contact WHERE status='0' ORDER BY id DESC";
 
                     $msq = $db->select($query);
-                    if($msq){ 
+                    if ($msq) {
                         $i = 0;
-                    while($result = $msq->fetch_assoc()){
-                        $i++
-                    ?>
-                    <tr class="odd gradeX"> 
-                        <td><?php echo $i; ?></td>
-                        <td><?php echo $result['firstname'].' '.$result['lastname']; ?></td> 
-                        <td><?php echo $result['email']; ?></td> 
-                        <td><?php echo $fm->textShorten($result['body'], 30); ?></td>  
-                        <td><?php echo $fm->formatDate($result['date']); ?></td>  
-                        <td>
-                            <a href="viewmsg.php?msgid=<?php echo $result['id'];?>">View</a> ||
-                            <a href="replaymsg.php?msgid=<?php echo $result['id'];?>">Reply</a> ||
-                            <a href="?seenid=<?php echo $result['id'];?>">Seen</a> ||
-                        </td>
-                    </tr>
-                    <?php  }  } ?>
+                        while ($result = $msq->fetch_assoc()) {
+                            $i++
+                                ?>
+                            <tr class="odd gradeX">
+                                <td>
+                                    <?php echo $i; ?>
+                                </td>
+                                <td>
+                                    <?php echo $result['firstname'] . ' ' . $result['lastname']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $result['email']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $fm->textShorten($result['body'], 30); ?>
+                                </td>
+                                <td>
+                                    <?php echo $fm->formatDate($result['date']); ?>
+                                </td>
+                                <td>
+                                    <a href="viewmsg.php?msgid=<?php echo $result['id']; ?>">View</a> ||
+                                    <a href="replaymsg.php?msgid=<?php echo $result['id']; ?>">Reply</a> ||
+                                    <a onclick="return confirm('Are You Sure to Move Message?')" href="?seenid=<?php echo $result['id']; ?>">Seen</a> ||
+                                </td>
+                            </tr>
+                        <?php }
+                    } ?>
                 </tbody>
             </table>
+
         </div>
     </div>
 
@@ -58,20 +88,40 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    $query = "SELECT * FROM tbl_contact WHERE status='1' ORDER BY id DESC";
 
-                    <tr class="odd gradeX">
-                        <td>01</td>
-                        <td>Name</td>
-                        <td>Email</td>
-                        <td>Message</td>
-                        <td>Date</td>
-                        <td>
-                            <a href="">Delete</a>
-                        </td>
-                    </tr>
-
+                    $msq = $db->select($query);
+                    if ($msq) {
+                        $i = 0;
+                        while ($result = $msq->fetch_assoc()) {
+                            $i++
+                                ?>
+                            <tr class="odd gradeX">
+                                <td>
+                                    <?php echo $i; ?>
+                                </td>
+                                <td>
+                                    <?php echo $result['firstname'] . ' ' . $result['lastname']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $result['email']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $fm->textShorten($result['body'], 30); ?>
+                                </td>
+                                <td>
+                                    <?php echo $fm->formatDate($result['date']); ?>
+                                </td>
+                                <td>
+                                    <a onclick="return confirm('Are You Sure to Delete?')" href="?delid=<?php echo $result['id']; ?>">Delete</a>
+                                </td>
+                            </tr>
+                        <?php }
+                    } ?>
                 </tbody>
             </table>
+
         </div>
     </div>
 
