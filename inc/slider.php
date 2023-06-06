de9246e71fd8c87d816b745363b896b04110a393
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-07 00:27:19 +0600

    Showing Sliders on Front View

--- inc/slider.php
+++ inc/slider.php
@@ -1,16 +1,17 @@
 <div class="slidersection templete clear">
 	<div id="slider">
-		<a href="#">
-			<img src="images/slideshow/01.jpg" alt="nature 1"
-				title="This is slider one Title or Description" />
-		</a>
+		<?php
+		$query = "SELECT * FROM tbl_slider ORDER BY id LIMIT 4"; 
+		$slider = $db->select($query);
 
+		if ($slider) { 
+			while ($result = $slider->fetch_assoc()) { 
+				?>
+				<a href="#">
+					<img src="admin/<?php echo $result['image'] ?>" alt="nature 1" title="<?php echo $result['title'] ?>" />
+				</a>
+			<?php }
+		} ?>
 
-		<a href="#"><img src="images/slideshow/02.jpg" alt="nature 2"
-				title="This is slider Two Title or Description" /></a>
-		<a href="#"><img src="images/slideshow/03.jpg" alt="nature 3"
-				title="This is slider three Title or Description" /></a>
-		<a href="#"><img src="images/slideshow/04.jpg" alt="nature 4"
-				title="This is slider four Title or Description" /></a>
 	</div>
 </div>
