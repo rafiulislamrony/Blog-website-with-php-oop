2f453324d32b32ceaa5ee4eae848eba3a8500bb7
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-09 12:09:03 +0600

    Adding Essential Files

--- config/config.php
+++ config/config.php
@@ -6,4 +6,3 @@ define("DB_NAME", "db_blog");
 define("TITLE", "Blog Site With Php Opp");
 define("KEYWORDS", "Blog Project, Php project, php opp, Database");
 
- 
--- search.php
+++ search.php
@@ -1,46 +1,51 @@
-
-<?php include 'inc/header.php'; ?> 
-<?php 
+<?php include 'inc/header.php'; ?>
+<?php
 
 $searchid = $db->link->real_escape_string($_GET['search']);
 
-if(!isset($searchid) || $searchid == NULL ){
-	header("Location:404.php");
-	exit;   
-}else{
-	$search = $searchid; 
+if (!isset($searchid) || $searchid == NULL) {
+    header("Location:404.php");
+    exit;
+} else {
+    $search = $searchid;
 }
 
-?> 
- 
+?>
+
 <div class="contentsection contemplete clear">
-	<div class="maincontent clear">
-    <?php 
+    <div class="maincontent clear">
+        <?php
 
         $query = "SELECT * FROM tbl_post WHERE title LIKE '%$search%' OR body LIKE '%$search%'";
 
-		$post = $db->select($query); 
-		if($post) {
-		while($result = $post->fetch_assoc()) {
-		?>
-    <div class="samepost clear">
-	
-    <h2><a href="post.php?id=<?php echo $result['id'] ?>"><?php echo $result['title'] ?> </a></h2>
-        <h4><?php echo $fm->formatDate($result['date']); ?> <a href="#"><?php echo $result['aurthor'] ?></a></h4>
-        <a href="post.php?id=<?php echo $result['id'] ?>"><img src="admin/<?php echo $result['image'] ?>" alt="post image" /></a>
-        <p>
-        <?php echo $fm->textShorten($result['body']); ?> 
-        </p>
-        <div class="readmore clear">
-        <a href="post.php?id=<?php echo $result['id'] ?>">Read More</a>
-        </div>
-    </div>
-    <?php }} else {  ?> 
-        <h3>Your Search Reasult Not Found ...</h3>
-		
-        <?php } ?> <!--- End While Loop -->
-        
-	</div>
+        $post = $db->select($query);
+        if ($post) {
+            while ($result = $post->fetch_assoc()) {
+                ?>
+                <div class="samepost clear">
 
-<?php include 'inc/sidebar.php'; ?>
-<?php include 'inc/footer.php'; ?>
+                    <h2><a href="post.php?id=<?php echo $result['id'] ?>"><?php echo $result['title'] ?> </a></h2>
+                    <h4>
+                        <?php echo $fm->formatDate($result['date']); ?> <a href="#">
+                            <?php echo $result['aurthor'] ?>
+                        </a>
+                    </h4>
+                    <a href="post.php?id=<?php echo $result['id'] ?>"><img src="admin/<?php echo $result['image'] ?>"
+                            alt="post image" /></a>
+                    <p>
+                        <?php echo $fm->textShorten($result['body']); ?>
+                    </p>
+                    <div class="readmore clear">
+                        <a href="post.php?id=<?php echo $result['id'] ?>">Read More</a>
+                    </div>
+                </div>
+            <?php }
+        } else { ?>
+            <h3>Your Search Reasult Not Found ...</h3>
+
+        <?php } ?> <!--- End While Loop -->
+
+    </div>
+
+    <?php include 'inc/sidebar.php'; ?>
+    <?php include 'inc/footer.php'; ?>
